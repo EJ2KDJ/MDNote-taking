025d40d37f691f1e8182521311ad9fb0a3c4b72b
EJ2KDJ <65149114+EJ2KDJ@users.noreply.github.com>
2025-12-01 23:43:56 +0800

    convert md to html endpoint

--- controllers/noteControler.js
+++ controllers/noteControler.js
@@ -1,9 +1,10 @@
 import fs from 'fs';
-import path from 'path';
+import path from 'path'; // note in brackets bc it's a core module meaning no need to install
+import { marked } from 'marked'; // In brackets because marked is not a default export
 import { v4 as uuidv4 } from 'uuid';
 
 // Create a new note
-const createNote = (req, res) => {
+const createNote = async (req, res) => {
     try {
         const { content } = req.body; // Get note content from request body
         if (!content) {
@@ -28,4 +29,33 @@ const createNote = (req, res) => {
 
 }
 
-export default { createNote };
+// Render MD note to HTML
+const renderNote = async (req, res) => {
+    try {
+        const { filename } = req.body; // get filename from request body. (Not params because it's a POST request)
+
+        // Error handling
+        if (!filename) {
+            return res.status(400).json({ message: "Note ID is required" });
+        }
+
+        const notePath = path.join('uploads', filename); // construct the file path
+
+        if (!fs.existsSync(notePath)) { // check if file exists
+            return res.status(404).json({ message: "Note not found" });
+        }
+
+        const content = fs.readFileSync(notePath, "utf-8") // read file in UTF-8 for proper text handling
+        const html = marked(content); // convert markdown to HTML (MAIN FUNCTIONALITY)
+
+        res.status(200).json({
+            filename,
+            html
+        })
+    } catch (err) {
+        console.error("Error rendering note:", err);
+        res.status(500).json({ message: "Internal server error" }); 
+    }
+}
+
+export default { createNote, renderNote };
--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -3,6 +3,10 @@ import noteController from "../controllers/noteControler.js";
 
 const router = express.Router();
 
+// Save notes
 router.post("/", noteController.createNote);
+
+// Render MD notes to HTML
+router.post("/render", noteController.renderNote);
 
 export default router;
